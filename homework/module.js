45845353224329ba1195b2cbf5ac5b1ed62f231d
marwo89 <marwo_89@yahoo.com>
2017-02-25 22:03:18 -0700

    made presentation page and updated franklin and greenville sites

---
+++ homework/module.js
@@ -0,0 +1,11 @@
+//jquery function to impliment moduralization
+
+$ (function (){
+
+$('#page-header').load('header.html');
+$('#page-nav').load('nav.html');
+$('#page-fiiter').load('footer.html');
+
+
+
+});
