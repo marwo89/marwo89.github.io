af6b8aa563ed778928f920e863d581526e4282d4
marwo89 <marwo_89@yahoo.com>
2017-03-17 21:14:12 -0600

    api activity

---
+++ jsonweather-html/scripts/modules.js
@@ -0,0 +1,7 @@
+// Include modules into the html file
+// Pathes may need altered depending on site structure
+$(function () {
+    $("#page-header").load("/marwo89.github.io/jsonweather-html/modules/header.html");
+    $("#page-nav").load("/marwo89.github.io/jsonweather-html/modules/navigation.html");
+    $("#footer-content").load("/marwo89.github.io/jsonweather-html/modules/footer.html");
+});
---
+++ jsonweather-html/scripts/remote_wu.js
@@ -0,0 +1,70 @@
+// Current Location Scripts
+$(function () {
+
+    var status = $('#status');
+
+    (function getGeoLocation() {
+        status.text('Getting Location...');
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var lat = position.coords.latitude;
+                var long = position.coords.longitude;
+
+                // Call the getData function, send the lat and long
+                getData(lat, long);
+
+            });
+        } else {
+            status.text("Your browser doesn't support Geolocation or it is not enabled!");
+        }
+
+    })();
+
+    // Get the data from the wunderground API
+    function getData(lat, long) {
+        $.ajax({
+            url: "//api.wunderground.com/api/174f87819a8d5765/geolookup/conditions/q/" + lat + "," + long + ".json",
+
+            dataType: "jsonp",
+            success: function (data){
+            console.log(data)
+
+            var location = data.location.city + ", " + data.location.state;
+                console.log(location)
+            var city = $('#cityDisplay');
+            var cityT = $('title');
+                city.text(location);
+                cityT.prepend(location + " ");
+            var temper = data.current_observation.temp_f;
+                var temp = $("#currentTemp");
+                temp.text(temper + "°");
+
+            var summary = data.current_observation.weather;
+                var summaryH = $('#summary');
+                summaryH.text(summary);
+
+            var wind = data.current_observation.wind_gust_mph;
+                var windH = $('#add1');
+                windH.text(wind + "mph");
+
+            var wdir = data.current_observation.wind_dir;
+                var wdirH = $('#add2');
+                wdirH.text('Wind Direciton:' + wdir)
+            var humid = data.current_observation.relative_humidity;
+                var humidH = $('#add3');
+                humidH.text('Humidity:' + humid);
+
+
+                $("#cover").fadeOut(250);
+            }
+        });
+
+}
+
+// A function for changing a string to TitleCase
+function toTitleCase(str) {
+    return str.replace(/\w+/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+});
